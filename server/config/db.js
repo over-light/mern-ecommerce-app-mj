2e26570f41cbf2514be85b2353ac85308cc60685
over.light <over.light888@gmail.com>
2023-05-05 16:24:19 +0530

    test

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const {
-  DB_USER_NAME, DB_USER_PASSWORD, DB_NAME, CLUSTER_URL, IS_DEVELOPMENT,
+  DB_USER_NAME, DB_USER_PASSWORD, DB_NAME, CLUSTER_URL,
 } = require('./env');
 
 const URI = `mongodb+srv://${DB_USER_NAME}:${DB_USER_PASSWORD}@${CLUSTER_URL}/${DB_NAME}?retryWrites=true&w=majority`;
 
-mongoose.connect(IS_DEVELOPMENT ? `mongodb://localhost:27017/${DB_NAME}` : URI).then(() => {
+mongoose.connect(URI).then(() => {
   console.log('Connected');
 }).catch((err) => {
   console.log('err:::', err);
