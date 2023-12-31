76a6219ce0a76f9aa6c82416ab127d61167df9a2
over.light <over.light888@gmail.com>
2024-01-01 00:32:15 +0530

    check mail code

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 const {EMAIL_FROM, EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_SERVICE } = require('../config/env');
 
+// eslint-disable-next-line consistent-return
 module.exports = async (to, subject, text, html) => {
   try {
     const transporter = nodemailer.createTransport({
@@ -17,11 +18,23 @@ module.exports = async (to, subject, text, html) => {
       text,
       html,
     };
+    await new Promise((resolve, reject) => { 
 
-    return transporter.sendMail(options, (error) => {
-      if (error) console.log({error,message:'email not sent!'});
-      else console.log('email sent successfully');
+      transporter.sendMail(options, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
     });
+
+     });
+    // return transporter.sendMail(options, (error) => {
+    //   if (error) console.log({error,message:'email not sent!'});
+    //   else console.log('email sent successfully');
+    // });
   } catch (error) {
     return error;
   }
