9c353854bf4ccf5ad5d085536af27bd4d21d2128
over.light <over.light888@gmail.com>
2024-01-01 13:22:26 +0530

    fix gmail mail issue

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,52 +1,30 @@
 const nodemailer = require('nodemailer');
-const {EMAIL_FROM, EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_SERVICE } = require('../config/env');
+const {EMAIL_SENDER, EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_SERVICE } = require('../config/env');
 
 module.exports = async (to, subject, text, html) => {
-  const transporter = nodemailer.createTransport({
-    service: EMAIL_SERVICE,
-    auth: {
-      user: EMAIL_USERNAME,
-      pass: EMAIL_PASSWORD,
-    },
-    secure: false,
-  });
-  
-  await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify((error, success) => {
-        if (error) {
-            console.log(error);
-            reject(error);
-        } else {
-            console.log('Server is ready to take our messages');
-            resolve(success);
-        }
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      service: EMAIL_SERVICE,
+      auth: {
+        user: EMAIL_USERNAME,
+        pass: EMAIL_PASSWORD,
+      },
+      secure:false,
     });
-  });
+    const options = {
+      from: EMAIL_SENDER,
+      to,
+      subject,
+      text,
+      html,
+    };
 
-  const mailData = {
-    from: {
-        name: EMAIL_FROM,
-        address:EMAIL_FROM,
-    },
-    replyTo: EMAIL_FROM,
-    to,
-    subject,
-    text,
-    html,
-  };
-
-  await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-            console.error(err);
-            reject(err);
-        } else {
-            resolve(info);
-        }
+    return transporter.sendMail(options, (error) => {
+      if (error) console.log({error,message:'email not sent!'});
+      else console.log('email sent successfully');
     });
-  });
-
+  } catch (error) {
+    return error;
+  }
 };
-
